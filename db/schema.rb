0c173e4877f0ffe3d0e54c1a51b9bea26e2773aa
“Pavel <itspavelnosarev@gmail.com>
2021-07-28 14:30:42 -0400

    fixed boolean to deafult to false

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_175735) do
   create_table "user_coins", force: :cascade do |t|
     t.integer "user_id"
     t.integer "coin_id"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -38,9 +38,8 @@ ActiveRecord::Schema.define(version: 2021_07_28_175735) do
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(metal: "silver", description: "Pre-War Dime Piece.", denomination: "10 cents", mint: "Pennsylvania", Year: "1905", image: https://i.ebayimg.com/images/g/G8oAAOSw5llf6M6~/s-l300.jpg)
+Coin.create!(metal: "silver", description: "Quarter with Standing Liberty design.", denomination: "25 cents", mint: "Denver", Year: 1921, image: https://dyn1.heritagestatic.com/lfset=path%5B3%2F6%2F1%2F4%2F3614006%5D%2Csizedata%5B850x600%5D&call=url%5Bfile%3Aproduct.chain%5D)
+Coin.create!(metal: "gold", description: "The Indian Head gold pieces or Pratt-Bigelow gold coins", denomination: "2 Dollars and 50 cents", mint: "Denver", Year: "1908", image: https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/NNC-US-1908-G%242%C2%BD-Indian_Head.jpg/475px-NNC-US-1908-G%242%C2%BD-Indian_Head.jpg)
+
+User.create!(username: "pavelnosarev", password: "password", confirmation: "password", admin: true, email "pavelnosarev@example.com")
+User.create!(username: "rwilroy", password: "password", confirmation: "password", admin: true, email "rwilroy@example.com")
+User.create!(username: "ebarbtrevor", password: "password", confirmation: "password", admin: false, email "pavelnosarev@example.com")
